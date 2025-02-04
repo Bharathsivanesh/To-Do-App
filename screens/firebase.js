bb7221225b118aec4246c9946c05c8c39b325b5a
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2025-02-04 22:03:19 +0530

    Update firebase.js

--- screens/firebase.js
+++ screens/firebase.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTT-_oSMMHOPx40VD1v9FAsbvS-T1TzqM",
-  authDomain: "to-do-14eee.firebaseapp.com",
-  projectId: "to-do-14eee",
-  storageBucket: "to-do-14eee.firebasestorage.app",
-  messagingSenderId: "756656362036",
-  appId: "1:756656362036:web:f6cfbf3710cb0951c27061",
+ //edit yout firebase config id
 };
 const app =getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const auth = initializeAuth(app, {
